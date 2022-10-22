790868985c709c55be66ed94e3b76411d93d4b55
crasgaitis <crasgaitis@gmail.com>
2022-10-22 15:44:59 -0700

    updates!!

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,5 @@ import pickle
 with open("model.pkl", 'rb') as file:
     model = pickle.load(file)
 
-st.title('yay')
+st.title('ML Model')
+st.write('Determine if your air quality is dangerously smoky!')

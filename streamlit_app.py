a7c124b408b1507d39cca937c55159be0faf8c60
crasgaitis <crasgaitis@gmail.com>
2022-10-22 15:55:24 -0700

    streamlit yuh

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,17 @@ with open("model.pkl", 'rb') as file:
 
 st.title('ML Model')
 st.write('Determine if your air quality is dangerously smoky!')
+
+@st.cache
+def load_data(file):
+    data = pd.read_csv(file)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    return data
+
+DATA = 'smoke_detection_iot.csv'
+data = load_data(DATA)
+
+max_temp = int(data["Temperature"].max())
+min_temp = 0
+temp = st.sidebar.slider("Temperature", int((min_temp+max_temp)/2))

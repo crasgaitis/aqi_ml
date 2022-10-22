ee79ff2300d274d6c978853bab38dbebfd85f260
crasgaitis <crasgaitis@gmail.com>
2022-10-22 15:14:52 -0700

    comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,10 @@ import pandas as pd
 import numpy as np
 import math
 from PIL import Image
-from pipeline import full_pipeline
+## from pipeline import full_pipeline
 import pickle
 
 with open("model.pkl", 'rb') as file:
     model = pickle.load(file)
+
+st.title('yay')
